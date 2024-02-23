c08eb3837ecd64aa0d915d475c41f61a45691929
Maxence Jules <142577996+Maxencejules@users.noreply.github.com>
2024-02-23 02:36:17 -0500

    Create dog_years.cpp

---
+++ dog_years.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+  // Sparkles is 6 years old
+  int dog_age = 3;
+  int early_years, later_years, human_years;
+  // The first two years
+  early_years = 21;
+  // After the two first years
+  later_years = (dog_age - 2) * 4;
+  human_years = early_years + later_years;
+  std::cout << "My name is Sparkles! Ruff ruff, I am " << human_years << " years old in human years.\n";
+
+return 0
+    
+  
+}
