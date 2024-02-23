28db7073180fcbd318fcf20b18ba745190bed5c3
Maxence Jules <142577996+Maxencejules@users.noreply.github.com>
2024-02-23 02:10:57 -0500

    Create initials.cpp

---
+++ initials.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  // Output initial
+  std::cout << "M     M  JJJJJ\n";
+  std::cout << "MM   MM    J  \n";
+  std::cout << "M M M M    J  \n";
+  std::cout << "M  M  M    J  \n";
+  std::cout << "M     M  JJJ  \n";
+  
+  return 0;
+}
